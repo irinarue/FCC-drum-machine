e3f966109e9dae090d5d40d9db4f3f02a09229a1
Irina Rüegg <rueegg.irina@gmail.com>
2024-09-01 08:29:48 +0200

    FIRST COMMIT all tests passed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,113 @@
-import logo from './logo.svg';
+import React, {Component} from "react";
 import './App.css';
 
-function App() {
+class DrumMachine extends Component {
+  constructor (props) {
+    super (props)
+    this.state = {
+      audios: [
+        {id: "Q",
+          name: "Heater 1",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-1.mp3",
+        },
+        {id: "W",
+          name: "Heater 2",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-2.mp3",
+        },
+        {id: "E",
+          name: "Heater 3",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-3.mp3",
+        },
+        {id: "A",
+          name: "Heater 4",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-4_1.mp3",
+        },
+        {id: "S",
+          name: "Clap",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Heater-6.mp3",
+        },
+        {id: "D",
+          name: "Open-HH",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Dsc_Oh.mp3",
+        },
+        {id: "Z",
+          name: "Kick-n'-Hat",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Kick_n_Hat.mp3",
+        },
+        {id: "X",
+          name: "Kick",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/RP4_KICK_1.mp3",
+        },
+        {id: "C",
+          name: "Closed-HH",
+          clip: "https://cdn.freecodecamp.org/testable-projects-fcc/audio/Cev_H2.mp3",
+        }
+      ],
+      currentAudio: { 
+        id: "",
+        name: "",
+        clip: ""
+      }
+    };
+    this.playAudio = this.playAudio.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+  }
+
+  componentDidMount () {
+    document.addEventListener("keydown", this.handleKeyPress);
+  }
+
+  componentWillUnmount () {
+    document.removeEventListener("keydown", this.handleKeyPress);
+  }
+
+  handleKeyPress (event) {
+    if(["Q", "W", "E", "A", "S", "D", "Z", "X", "C"].includes(event.key.toUpperCase())) {
+      const audio = this.state.audios.find((el) => el.id === event.key.toUpperCase());
+      this.playAudio(audio)
+    };
+  } 
+
+  playAudio (audio) {
+    this.setState(() => ({
+      currentAudio: audio
+    }));
+    const currAudioElement = document.getElementById(audio.id);
+    currAudioElement.play()
+  }
+    
+
+  render () {
+   const drumPads = this.state.audios.map((e)=>
+      <button 
+        className="drum-pad" 
+        id={e.name} 
+        key={e.name} 
+        onClick={() => this.playAudio(e)}>
+          {e.id}
+          <audio 
+            id={e.id} 
+            className="clip"
+            src={e.clip} 
+            type="audio/mp3" 
+            loop={false}>
+          </audio>
+        </button>
+    );
+
+    const audioName = this.state.currentAudio.name;
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>The Drum Machine</h1>
+      <p>Your forever favourite DJ!</p>
+      <div id="drum-machine">
+        <div id="drum-pads">{drumPads}</div>
+        <div id="display">{audioName}</div>
+      </div>
     </div>
   );
 }
+}
 
-export default App;
+export default DrumMachine;
